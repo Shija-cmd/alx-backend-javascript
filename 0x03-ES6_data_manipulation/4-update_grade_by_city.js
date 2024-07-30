f59c561f300e5d6ba530393687a9a6165739b0d1
Shija-cmd <shijajuma@protonmail.com>
2024-07-30 18:10:50 +0000

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,25 @@
-/* returns an array of students for a specific city with their new grade */
-function updateStudentGradeByCity(students, city, newGrades) {
-    const studentsInCity = students.filter((student) => student.location === city);
-    return studentsInCity.map((student) => {
-      const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
-      if (filteredGrades.length > 0) {
-        return {
-          ...student,
-          grade: filteredGrades[0].grade,
-        };
+export default function updateStudentGradeByCity(
+  students,
+  city,
+  newGrades,
+) {
+  const filterStudents = students.filter(
+    (student) => student.location === city,
+  );
+
+  const gradeStudents = filterStudents.map(
+    (student) => {
+      for (const gradeInfo of newGrades) {
+        if (student.id === gradeInfo.studentId) {
+          student.grade = gradeInfo.grade; 
+        }
       }
-      return {
-        ...student,
-        grade: 'N/A',
-      };
-    });
-  }
-  
-  export default updateStudentGradeByCity;
+      if (student.grade === undefined) {
+        student.grade = 'N/A'; 
+      }
+      return student;
+    },
+  );
+
+  return gradeStudents;
+}
